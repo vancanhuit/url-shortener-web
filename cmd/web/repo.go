e30e26028b0c2ddc48cf45953a044e15149c42cb
vancanhuit <vancanhuit@gmail.com>
2025-09-01 11:22:54 +0700

    chore: use database transactions

--- cmd/web/repo.go
+++ cmd/web/repo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var ErrRecordNotFound = errors.New("record not found")
@@ -12,6 +14,11 @@ type Repo struct {
 }
 
 func (r *Repo) Insert(url, alias string) (string, error) {
+	tx, err := r.DB.Begin()
+	if err != nil {
+		return "", err
+	}
+	defer tx.Rollback()
 	var existingAlias string
 	stmt := `WITH res AS (
 		INSERT INTO urls (original_url, alias) VALUES ($1, $2)
@@ -22,24 +29,42 @@ func (r *Repo) Insert(url, alias string) (string, error) {
 	SELECT alias FROM res
 	UNION ALL
 	SELECT alias FROM urls WHERE original_url = $1;`
-	err := r.DB.QueryRow(stmt, url, alias).Scan(&existingAlias)
-	if err != nil && err != sql.ErrNoRows {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err = tx.QueryRowContext(ctx, stmt, url, alias).Scan(&existingAlias)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return "", err
 	}
 	if existingAlias != "" {
 		alias = existingAlias
 	}
+
+	if err := tx.Commit(); err != nil {
+		return "", err
+	}
 	return alias, nil
 }
 
 func (r *Repo) GetOriginalURL(alias string) (string, error) {
+	tx, err := r.DB.Begin()
+	if err != nil {
+		return "", err
+	}
+	defer tx.Rollback()
 	var originalURL string
 	stmt := `SELECT original_url FROM urls WHERE alias = $1;`
-	err := r.DB.QueryRow(stmt, alias).Scan(&originalURL)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err = tx.QueryRowContext(ctx, stmt, alias).Scan(&originalURL)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return "", ErrRecordNotFound
 		}
+		return "", err
+	}
+	if err := tx.Commit(); err != nil {
 		return "", err
 	}
 	return originalURL, nil
